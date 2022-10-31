987fa24338407921d8685a44e526b18142f91f37
SamDD07 <samuel.dorismond@yahoo.com>
2022-10-31 10:19:52 +0100

    "ok"

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const issho = document.querySelector('.issho')
 const moi = document.querySelector('.moi')
 
+function elementSelected(e){
+    console.log(e.target.textContent);
+}
+
 function createResponse(content, reponse, interaction){
     const responseContent = document.createElement('div')
     responseContent.textContent = reponse
 
     if (interaction == true) {
         responseContent.classList.add("selection")
-        responseContent.addEventListener('click', function(){
-            this.style.color = "red";
+        responseContent.addEventListener('click', function(e){
+            elementSelected(e)
         })
     }
     eval(content).appendChild(responseContent)
@@ -18,3 +22,6 @@ createResponse("issho", "Bonjour !\nNous sommes issho, une communauté de dirige
 createResponse("issho", "Qui êtes vous ?")
 createResponse("moi", "Je suis ...")
 createResponse("moi", "Un dirigeant d'entreprise", true)
+createResponse("moi", "Un dirigeant d'une start-up", true)
+createResponse("moi", "Un cadre dirigeant", true)
+createResponse("moi", "Un directeur des Ressources", true)
