5f0895811cf0f69179e692ff92da31f5f9f89b76
Samuel Dorismond <samuel.dorismond@yahoo.com>
2022-11-01 13:56:35 +0100

    ok

--- index.js
+++ index.js
@@ -1,14 +1,61 @@
-const issho = document.querySelector('.issho')
-const moi = document.querySelector('.moi')
+const content = document.querySelector('.container-issho')
+
 let temps = 0
+let indiceElement = {
+    moi: 0,
+    issho: 0,
+    data: null
+}
+
+async function getData(){
+    await fetch('./Cheminement-Formulaire-Issho.json')
+    .then(res => res.json())
+    .then(data => createConversation(data))
+}
+
+function createConversation(tab){
+    indiceElement.data = tab[0].topic.topics
+    console.log(indiceElement.data);
+    createIssho()
+    createMoi()
+
+    createResponse(`.issho.n-${0}`, "Bonjour !\nNous sommes issho, une communauté de dirigeants et d’entrepreneurs qui vous aide à trouver des solutions concrètes à votre développement et à la croissance de votre entreprise.")
+    createResponse(`.issho.n-${0}`, "Qui êtes vous ?")
+    indiceElement.data.map((e, i) => createResponse(`.moi.n-${0}`, e.title, i, true))
+}
+
+getData()
+
+function createIssho(){
+    const issho = document.createElement('div')
+    issho.classList.add('issho', `n-${indiceElement.issho}`)
+    content.appendChild(issho)
+    indiceElement.issho += 1
+}
+
+function createMoi(){
+    const moi = document.createElement('div')
+    moi.classList.add('moi', `n-${indiceElement.moi}`)
+    content.appendChild(moi)
+    indiceElement.moi += 1
+}
 
 function elementSelected(e){
     temps = 0
-    moi.querySelectorAll('div.selection').forEach(e => moi.removeChild(e))
-    createResponse('moi', e.target.textContent)
-    createResponse('issho', "test")
-    Cheminement[e.target.id].sousElement.map((element, indice) =>
-        createResponse('moi', element.element, e.target.id+indice, true)
+    document.querySelector(`div.moi.n-${indiceElement.moi-1}`).outerHTML = ""
+    createMoi()
+    createResponse(`.moi.n-${indiceElement.moi-1}`, e.target.textContent)
+    
+    createIssho()
+    createResponse(`.issho.n-${indiceElement.issho-1}`, "Question à poser !")
+    
+    createMoi()
+
+    const test = searchInObj(indiceElement.data, "title", e.target.textContent).replace("title", "topics")
+    indiceElement.data = eval("indiceElement.data"+test)
+
+    indiceElement.data.map((element, indice) =>
+        createResponse(`.moi.n-${indiceElement.moi-1}`, element.title, e.target.id+indice, true)
     )
 }
 
@@ -16,7 +63,7 @@ function createResponse(content, reponse, id, interaction){
     const responseContent = document.createElement('div')
     responseContent.textContent = reponse
 
-    if (interaction == true) {
+    if (interaction) {
         responseContent.classList.add("selection")
         responseContent.id = id
         responseContent.addEventListener('click', elementSelected)
@@ -24,14 +71,29 @@ function createResponse(content, reponse, id, interaction){
     setTimeout(() => {
         responseContent.style.display= "inline-flex"
         responseContent.style.animation = "translate 1s"
-        eval(content).appendChild(responseContent)
+        document.querySelector(content).appendChild(responseContent)
     }, temps)
-    console.log(temps)
     temps = temps + 500
 }
 
-createResponse("issho", "Bonjour !\nNous sommes issho, une communauté de dirigeants et d’entrepreneurs qui vous aide à trouver des solutions concrètes à votre développement et à la croissance de votre entreprise.")
-createResponse("issho", "Qui êtes vous ?")
-Cheminement.map((element, indice) =>
-    createResponse('moi', element.element, indice, true)
-)
+function searchInObj(obj, name, val, currentPath){
+    currentPath = currentPath || ''
+
+    let matchingPath
+  
+    if (!obj || typeof obj !== 'object') return
+  
+    if (obj[name] === val) return `${currentPath}['${name}']`
+  
+    for (const key of Object.keys(obj)) {
+      if (key === name && obj[key] === val) {
+        matchingPath = currentPath
+      } else {
+        matchingPath = searchInObj(obj[key], name, val, `${currentPath}['${key}']`)
+      }
+  
+      if (matchingPath) break
+    }
+  
+    return matchingPath
+}
