edf32bca117a5db89e8d5a3fbff65aa46f57df6e
SamDD07 <samuel.dorismond@yahoo.com>
2022-11-03 16:42:42 +0100

    ok

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ let indiceElement = {
     issho: 0,
     data: null,
     top: window.scrollY,
+    clc: ["Discuter avec un expert", "Etre rappelé par un expert", "Prendre rendez-vous"],
     temps: 50
 }
 
 async function getData(){
-    await fetch('https://samuel-dd07.github.io/questionnaire-issho-partners/Cheminement-Formulaire-Issho.json')
+    await fetch('./Cheminement-Formulaire-Issho.json')
     .then(res => res.json())
-    
     .then(data => createConversation(data))
 }
 
@@ -45,28 +45,52 @@ function elementSelected(e){
     createMoi()
     createResponse(`.moi.n-${indiceElement.moi-1}`, e.target.textContent)
     document.querySelector(`div.moi.n-${indiceElement.moi-2}`).outerHTML = ""
+
+    const path = searchInObj(indiceElement.data, "title", e.target.textContent).replace("title", "topics")
+    indiceElement.data = eval("indiceElement.data"+path)
+
     
-    createIssho()
-    createResponse(`.issho.n-${indiceElement.issho-1}`, "Question à poser !")
+    if (!!indiceElement.data && indiceElement.data[0]["title"] === 'Question') {
+        createIssho()
+        if (!!indiceElement.data[0]["topics"][0]["topics"]) {
+            createResponse(`.issho.n-${indiceElement.issho-1}`, indiceElement.data[0]["topics"][0]["title"])
+            indiceElement.data = indiceElement.data[0]["topics"][0]["topics"]
+        } else {
+            indiceElement.data[0]["topics"].map((element, indice) => {
+                createResponse(`.issho.n-${indiceElement.issho-1}`, element.title)
+            })
+            indiceElement.data = null
+        }
+    } else {
+        console.log("test");
+        createIssho()
+        createResponse(`.issho.n-${indiceElement.issho-1}`, "Vous souhaitez en savoir plus ?")
+    }
     
     createMoi()
-
-    const test = searchInObj(indiceElement.data, "title", e.target.textContent).replace("title", "topics")
-    indiceElement.data = eval("indiceElement.data"+test)
-
     try{
         indiceElement.data.map((element, indice) =>
         createResponse(`.moi.n-${indiceElement.moi-1}`, element.title, e.target.id+indice, true)
         )
     } catch(e){
-        createResponse(`.moi.n-${indiceElement.moi-1}`, "Call To Action")
+        indiceElement.clc.map((element, indice) =>
+            createResponse(`.moi.n-${indiceElement.moi-1}`, element, "", true)
+        )
     }
 }
 
 function createResponse(content, reponse, id, interaction){
-    const responseContent = document.createElement('div')
-    responseContent.textContent = reponse
+    let responseContent = null
 
+    if (typeof reponse == "string" && reponse.indexOf(':') > -1){
+        responseContent = document.createElement('a')
+        responseContent.textContent = reponse.split('-')[0]
+        responseContent.href = reponse.split('-')[1]
+        interaction = false
+    } else {
+        responseContent = document.createElement('div')
+        responseContent.textContent = reponse
+    }
     if (interaction) {
         responseContent.classList.add("selection")
         responseContent.id = id
@@ -74,7 +98,7 @@ function createResponse(content, reponse, id, interaction){
     }
     setTimeout(() => {
         responseContent.style.display= "inline-flex"
-        responseContent.style.animation = "translate 1s ease"
+        responseContent.style.animation = "translate 1.5s ease"
         document.querySelector(content).appendChild(responseContent)
         indiceElement.top += responseContent.offsetHeight
         window.scroll({
