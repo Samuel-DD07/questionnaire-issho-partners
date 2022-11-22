63da7caef2c400ec52e6526649b83e815fce1ca4
SamDD07 <samuel.dorismond@yahoo.com>
2022-11-22 11:14:01 +0100

    ok

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 const content = document.querySelector('.container-issho')
 const button = document.querySelector('.button-start')
 
+// Tous les paramètres nécessaires pour le fonctionnement du script
 let indiceElement = {
     moi: 0,
     issho: 0,
-    data: null,
+    newData: null,
+    oldData: [],
     top: window.scrollY,
     clc: ["Discuter avec vous µ https://issho-partners.com/contact/"],
     temps: 50
 }
 
+// Pour récupérer les données du cheminement en format JSON
 async function getData() {
-     await fetch('https://samuel-dd07.github.io/questionnaire-issho-partners/Cheminement-Formulaire-Issho.json')
-         .then(res => res.json())
-         .then(data => createConversation(data))
+    await fetch('https://samuel-dd07.github.io/questionnaire-issho-partners/Cheminement-Formulaire-Issho.json')
+        .then(res => res.json())
+        .then(newData => createConversation(newData))
 }
 
+// Pour démarer la conversion et le script via les données JSON
 function createConversation(tab) {
     button.outerHTML = ""
-    indiceElement.data = tab[0].topic.topics
+    indiceElement.newData = tab[0].topic.topics
     createIssho()
     createMoi()
-
     createResponse(`.issho.n-${0}`, "Bonjour !\n\nnous allons vous poser quelques questions pour identifier les accompagnements les plus adapté à votre situation")
     createResponse(`.issho.n-${0}`, "Qui êtes vous ?")
-    indiceElement.data.map((e, i) => createResponse(`.moi.n-${0}`, e.title, i, true))
+    indiceElement.newData.map((e, i) => createResponse(`.moi.n-${0}`, e.title, i, true))
 }
 
-
+// Créer un block pour contenir les propos de issho
 function createIssho() {
     const issho = document.createElement('div')
     issho.classList.add('issho', `n-${indiceElement.issho}`)
@@ -35,6 +38,7 @@ function createIssho() {
     indiceElement.issho += 1
 }
 
+// Créer un block pour contenir les propos de l'utilisateur
 function createMoi() {
     const moi = document.createElement('div')
     moi.classList.add('moi', `n-${indiceElement.moi}`)
@@ -42,43 +46,72 @@ function createMoi() {
     indiceElement.moi += 1
 }
 
+// Pour gérer le comportement lorsqu'un block est selectionné par l'utilisateur.
 function elementSelected(e) {
     indiceElement.temps = 50
     createMoi()
     createResponse(`.moi.n-${indiceElement.moi - 1}`, e.target.textContent)
     document.querySelector(`div.moi.n-${indiceElement.moi - 2}`).outerHTML = ""
+    console.log(indiceElement.moi);
 
-    const path = searchInObj(indiceElement.data, "title", e.target.textContent).replace("title", "topics")
-    indiceElement.data = eval("indiceElement.data" + path)
 
-    if (!!indiceElement.data && indiceElement.data[0]["title"] === 'Question') {
-        createIssho()
-        if (!!indiceElement.data[0]["topics"][0]["topics"]) {
-            createResponse(`.issho.n-${indiceElement.issho - 1}`, indiceElement.data[0]["topics"][0]["title"])
-            indiceElement.data = indiceElement.data[0]["topics"][0]["topics"]
-        } else {
-            indiceElement.data[0]["topics"].map((element, indice) => {
-                createResponse(`.issho.n-${indiceElement.issho - 1}`, element.title)
-            })
-            indiceElement.data = null
-        }
-    } else {
-        createIssho()
-        createResponse(`.issho.n-${indiceElement.issho - 1}`, "Vous souhaitez en savoir plus ?")
-    }
+    if (e.target.textContent == "Revenir à la question précedente") {
+        const allMoi = document.querySelectorAll('div.moi')
+        const allIssho = document.querySelectorAll('div.issho')
+        allMoi[allMoi.length - 1].outerHTML = ""
+        allIssho[allIssho.length - 1].outerHTML = ""
+        allMoi[allMoi.length - 2].outerHTML = ""
 
-    createMoi()
-    try {
-        indiceElement.data.map((element, indice) =>
+        indiceElement.newData = indiceElement.oldData[indiceElement.oldData.length - 1]
+        indiceElement.oldData.pop()
+        indiceElement.moi -= 3
+        indiceElement.issho -= 1
+        createMoi()
+        indiceElement.newData.map((element, indice) =>
             createResponse(`.moi.n-${indiceElement.moi - 1}`, element.title, e.target.id + indice, true)
         )
-    } catch (e) {
-        indiceElement.clc.map((element, indice) =>
-            createResponse(`.moi.n-${indiceElement.moi - 1}`, element, "", true)
-        )
+        if (indiceElement.oldData.length > 0) {
+            createResponse(`.moi.n-${indiceElement.moi - 1}`, "Revenir à la question précedente", "précédent", true)
+        }
+    } else {
+        const path = searchInObj(indiceElement.newData, "title", e.target.textContent).replace("title", "topics")
+        indiceElement.oldData.push(indiceElement.newData)
+        indiceElement.newData = eval("indiceElement.newData" + path)
+
+        if (!!indiceElement.newData && indiceElement.newData[0]["title"] === 'Question') {
+            createIssho()
+            if (!!indiceElement.newData[0]["topics"][0]["topics"]) {
+                createResponse(`.issho.n-${indiceElement.issho - 1}`, indiceElement.newData[0]["topics"][0]["title"])
+                indiceElement.newData = indiceElement.newData[0]["topics"][0]["topics"]
+            } else {
+                indiceElement.newData[0]["topics"].map((element, indice) => {
+                    createResponse(`.issho.n-${indiceElement.issho - 1}`, element.title)
+                })
+                indiceElement.newData = null
+            }
+        } else {
+            createIssho()
+            createResponse(`.issho.n-${indiceElement.issho - 1}`, "Vous souhaitez en savoir plus ?")
+        }
+
+        createMoi()
+        try {
+            indiceElement.newData.map((element, indice) =>
+                createResponse(`.moi.n-${indiceElement.moi - 1}`, element.title, e.target.id + indice, true)
+            )
+            if (indiceElement.oldData.length > 0) {
+                createResponse(`.moi.n-${indiceElement.moi - 1}`, "Revenir à la question précedente", "précédent", true)
+            }
+        } catch (e) {
+            indiceElement.clc.map((element) =>
+                createResponse(`.moi.n-${indiceElement.moi - 1}`, element, "", true)
+            )
+            createResponse(`.moi.n-${indiceElement.moi - 1}`, "Revenir à la question précedente", "précédent", true)
+        }
     }
 }
 
+// Pour créer une réponse selon son texte, sa destination et son interaction
 function createResponse(content, reponse, id, interaction) {
     let responseContent = null
 
@@ -87,7 +120,7 @@ function createResponse(content, reponse, id, interaction) {
         responseContent.textContent = reponse.split('µ')[0]
         responseContent.href = reponse.split('µ')[1]
         interaction = false
-    } else if (typeof reponse == "string" && reponse.indexOf('£') > -1){
+    } else if (typeof reponse == "string" && reponse.indexOf('£') > -1) {
         responseContent = document.createElement('div')
         responseContent.textContent = reponse.split('£')[1]
         responseContent.classList.add("list")
@@ -105,20 +138,20 @@ function createResponse(content, reponse, id, interaction) {
         responseContent.style.animation = "translate 1.5s ease"
         try {
             document.querySelector(content).appendChild(responseContent)
-        } catch(e){}
-        console.log(window.innerHeight*3/4, responseContent.offsetTop)
-        if (window.innerHeight*2/4 < responseContent.offsetTop) {
-           indiceElement.top = window.scrollY + responseContent.offsetHeight
-           window.scroll({
+        } catch (e) { }
+        if (window.innerHeight * 2 / 4 < responseContent.offsetTop) {
+            indiceElement.top = window.scrollY + responseContent.offsetHeight * 1.5
+            window.scroll({
                 top: indiceElement.top,
                 behavior: 'smooth'
             })
         }
-         
+
     }, indiceElement.temps)
     indiceElement.temps = indiceElement.temps + 500
 }
 
+// Pour parcourir un objet ou un document json à l'aide d'une clé et une valeur données et obtenir son chemin.
 function searchInObj(obj, name, val, currentPath) {
     currentPath = currentPath || ''
     let matchingPath
